813f9f98cfe28badf118f849f3883d5e9ea09c26
Junhwan Jang <junhwan16@gmail.com>
2018-01-18 17:28:02 +0900

    neual-style-transfer alpha0.1

---
+++ utils.py
@@ -0,0 +1,68 @@
+# Most code in this file was borrowed from https://github.com/hwalsuklee/tensorflow-style-transfer
+
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL.Image
+import scipy.misc
+
+"""Helper-functions for image manipulation"""
+# This function loads an image and returns it as a numpy array of floating-points.
+# The image can be automatically resized so the largest of the height or width equals max_size.
+# or resized to the given shape
+def load_image(filename, shape=None, max_size=None):
+    img = scipy.misc.imread(filename).astype(np.float)
+    if len(img.shape) == 2:
+        # grayscale
+        img = np.dstack((img,img,img))
+    elif img.shape[2] == 4:
+        # PNG with alpha channel
+        img = img[:,:,:3]
+        
+    return img
+
+# Save an image as a jpeg-file.
+# The image is given as a numpy array with pixel-values between 0 and 255.
+def save_image(image, filename):
+    # Ensure the pixel-values are between 0 and 255.
+    image = np.clip(image, 0.0, 255.0)
+
+    # Convert to bytes.
+    image = image.astype(np.uint8)
+
+    # Write the image-file in jpeg-format.
+    with open(filename, 'wb') as file:
+        PIL.Image.fromarray(image).save(file, 'jpeg')
+
+# This function plots the content-, mixed- and style-images.
+def plot_images(content_image, style_image, mixed_image):
+    # Create figure with sub-plots.
+    fig, axes = plt.subplots(1, 3, figsize=(10, 10))
+
+    # Adjust vertical spacing.
+    fig.subplots_adjust(hspace=0.1, wspace=0.1)
+
+    # Plot the content-image.
+    # Note that the pixel-values are normalized to
+    # the [0.0, 1.0] range by dividing with 255.
+    ax = axes.flat[0]
+    ax.imshow(content_image / 255.0, interpolation='sinc')
+    ax.set_xlabel("Content")
+
+    # Plot the mixed-image.
+    ax = axes.flat[1]
+    ax.imshow(mixed_image / 255.0, interpolation='sinc')
+    ax.set_xlabel("Output")
+
+    # Plot the style-image
+    ax = axes.flat[2]
+    ax.imshow(style_image / 255.0, interpolation='sinc')
+    ax.set_xlabel("Style")
+
+    # Remove ticks from all the plots.
+    for ax in axes.flat:
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
